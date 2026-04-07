04b23d5495e3f015252cc3313ca32dbd94a701fc
Chirag440-gupta <guptachirag111433@gmail.com>
2026-04-08 01:06:36 +0530

    Time: 0 ms (100%), Space: 46.8 MB (51.45%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -3,11 +3,10 @@ class Solution {
         int n=nums.length;
         int slw=0;
         for(int i=1;i<n;i++){
-            if(nums[i]!=nums[slw]){
-                slw++;
-                nums[slw]=nums[i];
+            if(nums[slw]!=nums[i]){ 
+                nums[++slw]=nums[i];
             }
         }
-        return slw+1;
+        return ++slw;
     }
 }
