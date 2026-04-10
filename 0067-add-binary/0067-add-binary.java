fc04b5f38c11a4d2dd448ec39a005f94d01b062f
Chirag440-gupta <guptachirag111433@gmail.com>
2026-04-10 19:36:31 +0530

    Time: 2 ms (51.05%), Space: 43.5 MB (82.8%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -4,11 +4,10 @@ class Solution {
         int i=a.length()-1;
         int j=b.length()-1;
         int c=0;
-
         while(i>=0 || j>=0 || c!=0){
             int sum=c;
-            if(i>=0) sum+=a.charAt(i--) - '0';
-            if(j>=0) sum+=b.charAt(j--) - '0';
+            if(i>=0) sum+=a.charAt(i--) -'0';
+            if(j>=0) sum+= b.charAt(j--) - '0';
             sb.append(sum%2);
             c=sum/2;
         }
