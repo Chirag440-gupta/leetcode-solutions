fa682b904a537b7a77a7c54a783d2c7d1d02e00e
Chirag440-gupta <guptachirag111433@gmail.com>
2026-04-16 13:26:19 +0530

    Time: 1 ms (99.86%), Space: 43 MB (75.83%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,9 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char res=0;
+        for(char a:s.toCharArray()) res^=a;
+        for(char b:t.toCharArray()) res^=b;
+
+        return res;
+    }
+}
