4db7062bd34869647ab7c73fea09c8091a6e488a
Chirag Gupta <guptachirag111433@gmail.com>
2026-04-25 01:45:21 +0530

    Time: 1 ms (98.03%), Space: 42.6 MB (46.12%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int addDigits(int num) {
+        while(num>=10){
+            int a=0;
+              while( num>0){
+                a+=num%10;
+                num/=10;
+              }
+              num=a;
+        }
+        return num;
+    }
+}
