26dfbe10007c7b3286dd44a978794fa6f60545ef
Chirag Gupta <guptachirag111433@gmail.com>
2026-04-29 02:13:41 +0530

    Time: 1 ms (100%), Space: 43 MB (82.4%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,16 +1,16 @@
 class Solution {
     public int longestPalindrome(String s) {
-        int[] freq=new int[128];
-        for(char ch:s.toCharArray()){
+         int[] freq=new int[128];
+         for(char ch:s.toCharArray()){
             freq[ch]++;
-        }
-        int len=0;
-        for(int c:freq){
+         }
+         int len=0;
+         for(int c:freq){
             len+=(c/2)*2;
-        }
-            if(len < s.length()){
-                len+=1;
-        }
-        return len;
+         }
+         if(len < s.length()){
+            len+=1;
+         }
+         return len;
     }
 }
