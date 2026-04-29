4aa0ee090e8a566e2034c0a046fd6de49092f37c
Chirag Gupta <guptachirag111433@gmail.com>
2026-04-30 05:13:53 +0530

    Time: 2 ms (99.65%), Space: 72.3 MB (84.68%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int f=0;
+        int t=0;
+     for(int b:bills){
+        if(b==5) {
+            f++;
+            } else if(b==10){
+                if (f < 1) return false; 
+                f--;
+                t++;
+            } else {
+                if(t>0 && f>0){
+                    t--;
+                    f--;
+                }
+               else if (f >= 3) { 
+                f-=3;
+            }
+            else return false;
+            }
+     } 
+     return true;
+    }
+}
