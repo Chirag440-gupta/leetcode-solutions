b7cf9b4faa0ac810b0a82adeff6a783a964f42ab
Chirag Gupta <guptachirag111433@gmail.com>
2026-05-22 17:07:49 +0530

    Time: 1 ms (100%), Space: 42.8 MB (75.23%) - LeetHub

--- 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.java
+++ 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.java
@@ -2,15 +2,11 @@ class Solution {
     public int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
         int n1=numOnes,n2=numZeros,n3=numNegOnes;
         int n=n1+n2+n3;
-        int s=0;
-        int[] arr=new int[n];
-        Arrays.fill(arr,0,n1,1);
-        Arrays.fill(arr,n1,n1+n2,0);
-        Arrays.fill(arr,n1+n2,arr.length,-1);
-
-        for(int i=0;i<k;i++){
-            s+=arr[i];
-        }
-        return s;
+     if(k<=n1) return k;
+     else if(k<=n1+n2) return n1;
+     else{
+        int c=k-(n1+n2);
+        return n1-c;
+     }
     }
 }
