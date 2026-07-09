85bb4edfe8a65b48a00119c64d570d49fbbb983c
Chirag Gupta <guptachirag111433@gmail.com>
2026-07-10 00:46:05 +0530

    Time: 17 ms (80.92%), Space: 49.9 MB (17.14%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -3,7 +3,7 @@ class Solution {
         Arrays.sort(nums);
         int s=0;
         int n=nums.length;
-        for(int i=0;i<n-1;i+=2){
+        for(int i=0;i<n;i+=2){
             s+=nums[i];
         }
         return s;
